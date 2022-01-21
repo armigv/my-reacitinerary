f318c54f3d8a94c1d533e1b8e238bf9c491cdf19
Armando <armando13.133@gmail.com>
2022-01-21 14:53:50 -0300

    m

---
+++ my-app/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+ const Foo = () => {
+return(
+
+  <><div className="awesome" style={{ border: "1px solid red" }}>
+    <label htmlFor="name">Enter your name: </label>
+    <input type="text" id="name" />
+  </div><p>Enter your HTML here</p></>
+)
+ }
+  export default Foo
