51bd38ded88f4070006fa492e44992091189bca1
Armando <armando13.133@gmail.com>
2022-01-22 20:21:03 -0300

    infinit carousel

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import { BrowserRouter, Router, Route } from 'react-router-dom';
+
 import Footer from './components/footer';
 import City from './components/pages/city';
 import Home from './components/pages/home';
 import Navbar from './components/Navbar';
 import Carousel from './components/carousel';
+
+
 
 function App() {
   return (
--- my-app/src/components/carousel.js
+++ my-app/src/components/carousel.js
@@ -1,17 +1,23 @@
 import React from "react"
- 
+import Slider from 'infinite-react-carousel';
 
-const Carousel = () => {
+
+const Carousel = ({images}) => {
 
 return (
-<div>
+<section className='slider'>
+			<h1 className='slider__title'>Carousel con infinite-react-carousel</h1>
+			<Slider className='slider__content'>
+				{images.map((image) => (
+					<div key={image.id} className='slider__content--item'>
+						<img src={image.image} alt={image.title} />
+						<p className='slider-description'>{image.title}</p>
+					</div>
+				))}
+			</Slider>
+		</section>
+	
+	)
+}
 
-
-</div>
-
-)
-
-
-
-} 
 export default Carousel
