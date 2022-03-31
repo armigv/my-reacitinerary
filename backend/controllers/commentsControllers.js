f5508e3ad2583200cd4fc6dda6c10dd3e3cf414c
Armando <armando13.133@gmail.com>
2022-03-31 18:22:36 -0300

    comments

--- backend/controllers/commentsControllers.js
+++ backend/controllers/commentsControllers.js
@@ -5,42 +5,103 @@ const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { response } = require("express");
 
-const commentController= {
+const commentController = {
+  crearComentarios: async (req, res) => {
+    const { itinerary, user, message } = req.body.dataComments;
+
+    new Comments({
+      itineraries: itinerary,
+      user: user,
+      comment: message,
+    }).save();
+
+    let comentario;
+    try {
+      comentario = await Comments.find({ itineraries: itinerary }).populate(
+        "user"
+      );
+    } catch (err) {
+      console.log(error);
+    }
+    res.json({
+      success: true,
+      response:{comentario},
+    });
+
+    await axios
+      .post("http://localhost:4000/api/signIn", { dataComments })
+      .then((response) => setComment(response.data.response.commentario));
+  },
 
 
-GetAllItineraries: async (req, res) => {
-    
-    const {itinerary,user,message} = req.body.dataComments
-
-  new Comments({
-itineraries:itinerary
-user:user
-comments:message
-
-  }).save()
-
-
-  let Comments
-  try {
-      Comments = await Comments.find({itineraries:itinerary}).populate("user")
-  } catch (err) {
-      
 
 
 
-      console.log(error)
+  obtenerComentarios: async (req, res) => {
+    const { id} = req.params.id;
+
+    let comentario;
+    try {
+      comentario = await Comments.find({ itineraries: id}).populate(
+        "user"
+      );
+    } catch (err) {
+      console.log(error);
+    }
+    res.json({
+      success: true,
+      response:{comentario},
+    });
+
+  },
+
+
+
+  
+  borrarComentarios: async (req, res) => {
+    const { id} = req.params.id;
+
+    let comentario;
+    try {
+      comentario = await Comments.findOneAndDelete({ _id: id}).populate(
+        "user"
+      );
+    } catch (err) {
+      console.log(error);
+    }
+    res.json({
+      success: true,
+      response:{comentario},
+    });
+
+  },
+
+
+
+
+  
+  
+  modificarComentarios: async (req, res) => {
+    const { id} = req.params.id;
+
+    let comentario;
+    try {
+      comentario = await Comments.findOneAndUpdate({ _id: id}).populate(
+        "user"
+      );
+    } catch (err) {
+      console.log(error);
+    }
+    res.json({
+      success: true,
+      response:{comentario},
+    });
+
   }
-res.json({
-    success:true
-        response:{Comment},
-       
-    })
 
-     
-    await axios.post("http://localhost:4000/api/signIn",{dataComments})
-    .then(response=>
-        
-        setComment(response.data.response.commentario)
-        )
 
-}
+  
+
+
+};
+module.exports = commentController;
