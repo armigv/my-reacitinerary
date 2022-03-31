cb38c6a25d95059ca42a011c2172b7dbd88fee6d
Armando <armando13.133@gmail.com>
2022-03-31 18:24:40 -0300

    grillar cards

--- my-app/src/components/CitiesCards.js
+++ my-app/src/components/CitiesCards.js
@@ -1,31 +1,52 @@
 import React from "react";
+import "./Css/box.css"
 import { Link as LinkRouter } from "react-router-dom";
 import { useStateValue } from "../StateProvider";
-import "./Css/citiesfather.css"
+import "./Css/citiesfather.css";
 
 function Cards() {
   const [{ cities }, dispatch] = useStateValue();
   console.log(cities);
 
   return (
-    
-    <div className="wrapper">
-      {cities.map((city) => (
-        <div key={city._id} className="card__content-item">
-          <div className="card">
-            <div className="card__body">
-              <img src={process.env.PUBLIC_URL + `/imagenes/cities/${city.img}`}  className="card__image" alt={city.name} />
-              <h2 className="card__title">{city.name}</h2>
-              <p className="card__description">{city.description}</p>
+    <div>
+      <div className="grid">
+    {cities.map((city) => (
+    <div className="container">
+       
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div className="box">
+            <div className="card" style={{ border: "width:18rem;" }} >
+              <img
+                src={process.env.PUBLIC_URL + `/imagenes/cities/${city.img}`}
+                className="card-img-top"
+                alt={city.name}
+              />
+              <div className="card-body">
+                <div key={city._id} className="card-content-item">
+                  <h2 className="card-title">{city.name}</h2>
+                  <p className="card-text">{city.description}</p>
+                </div>
+                <LinkRouter to={`/ciudad/${city._id}`}>
+                  <button className="btn btn-primary">View Recipe</button>
+                </LinkRouter>
+              </div>
             </div>
-            <LinkRouter to ={`/ciudad/${city._id}`}>
-            <button className="card__btn">View Recipe</button>
-            </LinkRouter>
-          </div>
-          
+         
         </div>
+      </div>
+     </div>
+   
       ))}
-    </div>
+       </div>
+
+
+
+  
+      </div>
+
+
+
   );
 }
 
